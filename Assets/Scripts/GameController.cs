772bd31e3ca180de2f2eb4434a98d210fe3a215f
Djordje Cvijic <djordjec@nordeus.com>
2019-01-26 20:54:44 +0100

    Enemies spawning

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
+	public float enemySpawnPeriod = 1;
+	public GameObject enemyPrefab;
+	public float spawnDistance = 1;
+	public Transform earth;
+
+	private float timeSinceLastEnemy;
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +17,16 @@ public class GameController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		timeSinceLastEnemy += Time.deltaTime;
+		if (timeSinceLastEnemy >= enemySpawnPeriod && enemyPrefab != null && earth != null)
+		{
+			timeSinceLastEnemy -= enemySpawnPeriod;
+			var position = spawnDistance * Random.insideUnitCircle.normalized;
+			var position3 = new Vector3(position.x, position.y, 0);
+			var rotation = Quaternion.FromToRotation(Vector3.up, earth.position - position3);
+			Instantiate(enemyPrefab, position3, rotation);
+		}
 	}
 }
