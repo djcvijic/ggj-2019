1f3859325aa3579b2d3cdf7a5f6625a38382c837
BozidarVucevic <dzulijan111@gmail.com>
2019-01-26 22:16:59 +0100

    magicNumber

--- Assets/Scripts/Robit.cs
+++ Assets/Scripts/Robit.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Robit : MonoBehaviour
 {
 	public float moveSpeed = 1;
+	public float SecBetweenBullets= 0.25f;
 	public GameObject bulletPrefab;
 	public Transform gunBarrel;
 	private float timer;
+
 
 	// Use this for initialization
 	void Start () {
@@ -24,8 +26,8 @@ public class Robit : MonoBehaviour
 		var shoot = Input.GetButton("Pew");
 		if (shoot && (bulletPrefab != null) && (gunBarrel != null) && ((timer <= 0) || Input.GetButtonDown("Pew")) )
 		{
-			Instantiate(bulletPrefab, gunBarrel.position, transform.rotation, GameController.Instance.transform);
-			timer = 0.25f;
+			Instantiate(bulletPrefab, gunBarrel.position, transform.rotation);
+			timer = SecBetweenBullets;
 		}
 	}
 }
