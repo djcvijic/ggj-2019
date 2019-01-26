71f0a553f3db61ee1590c3e9a9affff8558af179
Djordje Cvijic <djordjec@nordeus.com>
2019-01-26 23:39:23 +0100

    difficulty curve

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,24 +10,37 @@ public class GameController : MonoBehaviour
 	public GameObject enemyPrefab;
 	public float spawnDistance = 1;
 	public Transform earth;
+	public float difficultyCurveSpeed = 1;
+	public AnimationCurve difficultyCurve;
 
 	public static GameController Instance;
 
+	private float timeSinceInitialization;
 	private float timeSinceLastEnemy;
 
 	// Use this for initialization
 	void Start ()
 	{
 		Instance = this;
+		timeSinceInitialization = 0;
+		timeSinceLastEnemy = 0;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		timeSinceLastEnemy += Time.deltaTime;
-		if (timeSinceLastEnemy >= enemySpawnPeriod && enemyPrefab != null && earth != null)
+		var actualSpawnPeriod = enemySpawnPeriod;
+		if (difficultyCurve != null)
 		{
-			timeSinceLastEnemy -= enemySpawnPeriod;
+			timeSinceInitialization += Time.deltaTime;
+			var difficultyTime = timeSinceInitialization * difficultyCurveSpeed;
+			actualSpawnPeriod *= 1 - difficultyCurve.Evaluate(difficultyTime);
+		}
+
+		timeSinceLastEnemy += Time.deltaTime;
+		if (timeSinceLastEnemy >= actualSpawnPeriod && enemyPrefab != null && earth != null)
+		{
+			timeSinceLastEnemy -= actualSpawnPeriod;
 			var position = spawnDistance * Random.insideUnitCircle.normalized;
 			var position3 = new Vector3(position.x, position.y, 0);
 			var rotation = Quaternion.FromToRotation(Vector3.up, earth.position - position3);
